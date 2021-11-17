f1740314a24743aaa70cfd2b6e9944e90fa45025
braiso-22 <braisfv22@gmail.com>
2021-11-17 18:11:52 +0100

    vista aviones empezada

---
+++ app/src/main/java/com/example/gestionvuelos/FligthsActivity.java
@@ -0,0 +1,14 @@
+package com.example.gestionvuelos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FligthsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fligths);
+    }
+}
