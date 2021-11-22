1506ea72d37c51a8cb204f8926b632b5424f6212
Brais <81094589+braiso-22@users.noreply.github.com>
2021-11-22 01:49:12 +0100

    Agregado a Firebase y splash screen creado con themes

--- app/src/main/java/com/example/gestionvuelos/MainActivity.java
+++ app/src/main/java/com/example/gestionvuelos/MainActivity.java
@@ -4,15 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent cambio = new Intent(this,LoginActivity.class);
+        Intent cambio = new Intent(this, FligthsActivity.class);
         startActivity(cambio);
     }
 }
